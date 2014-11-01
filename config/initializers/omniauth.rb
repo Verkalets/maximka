045d03da3cec3b6f86483e5f00b72270b66186f3
Verkalets <verkalets@hotmail.com>
2014-11-01 17:27:25 +0200

    add facebook auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret
+  provider :facebook, '1499552633639161', '2ad1380d9b66a11457ac1351e0975afb'
 end
