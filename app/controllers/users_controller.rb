74cef853ba7619048f06b83c501622db951729d4
Verkalets <verkalets@hotmail.com>
2014-11-02 12:51:00 +0200

    get friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,21 @@
+#require "fb_graph2"
+
 class UsersController < ApplicationController
   before_filter :authenticate_user!
   before_filter :correct_user?, :except => [:index]
 
   def index
     @users = User.all
+
+
   end
 
   def show
     @user = User.find(params[:id])
+    @graph = FbGraph2::User.new(@user.uid).authenticate(session[:auth_token]).fetch
+
+    @graph = @graph.friends.summary['total_count']
+
   end
 
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1499552633639161', '2ad1380d9b66a11457ac1351e0975afb'
+  provider :facebook, '1499552633639161', '2ad1380d9b66a11457ac1351e0975afb', :scope => 'public_profile,email,user_birthday,read_stream,user_friends'
 end
