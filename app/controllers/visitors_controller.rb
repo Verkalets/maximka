e6ea04719c1e2ebdd667514fb3ef3c60964f3841
Verkalets <verkalets@hotmail.com>
2014-11-15 15:29:37 +0200

    add mikrotik respond

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -7,31 +7,16 @@ class VisitorsController < ApplicationController
     MTik::verbose = true
 
     # Connect to the device:
-    connection = MTik::Connection.new :host => '192.168.0.200', :user => 'api', :pass => 'rf9kq)pL!'
-    $reply_limit = 10
-
-    # Execute the command:
-    $reply_count = 0
-    connection.get_reply_each(
-      "/interface/monitor-traffic",
-      "=interface=ether1",
-      "=.proplist=rx-bits-per-second,tx-bits-per-second"
-    ) do |request_object, reply_sentence|
-      if reply_sentence.key?('!re')  # We only pay attention to reply sentences
-        # Print the reply sentence:
-        p reply_sentence
-
-        # Increment the reply counter:
-        $reply_count += 1
-
-        # If we've reached our reply goal, cancel:
-        if $reply_count >= $reply_limit
-          # Cancel this command request:
-          request_object.cancel
-        end
-      end
-    end
-
-    connection.close
+    p connection = MTik::command(
+      :host    => '192.168.0.200',
+      :user    => 'api',
+      :pass    => 'rf9kq)pL!',
+      :command => [
+        "/interface/monitor-traffic",
+        "=interface=ether0",
+        "=.proplist=rx-bits-per-second,tx-bits-per-second"
+      ],
+      :limit => 10
+      )
   end
 end
