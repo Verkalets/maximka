4cf9f05ca059f184fbe65cb3d8ae0931b4c36029
Verkalets <verkalets@hotmail.com>
2014-12-07 15:10:19 +0200

    fix social login

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   
   
   get '/auth/facebook/callback' => 'sessions#create'
+  get '/auth/vkontakte/callback' => 'sessions#vkontakte'
   get '/signin' => 'sessions#new', :as => :signin
   get '/signin_vk' => 'sessions#new_vk', :as => :signin_vk
   get '/signout' => 'sessions#destroy', :as => :signout
